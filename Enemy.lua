3ec7b84f7177c2cb4ee4e7f7cbaf848df4a61249
IVHM <hmatthyseniv@gmail.com>
2020-02-01 01:08:58 -0600

    Add: Function to convert normal to string direction

--- Enemy.lua
+++ Enemy.lua
@@ -1,14 +1,4 @@
---enemy class
-
-Enemy = {
-	pos = {x=0,y=0},
-	normal = {0,-1}
-
-
-	
-	}
-	 
-
+--enemy class 
 
 --Enemy sprites are created out of individual pixels 
 --they are drawn based off of references to a grid atlas
@@ -16,6 +6,8 @@ Enemy = {
 --ie enemy_shape = {2,4,6} = X X
 
 
+---------------------
+--PIXEL SPRITE TABLES
 pixel_atlas = {{-1,-1},{0,-1},{1,-1},
 			   {-1, 0},{0, 0},{1, 0},
 			   {-1, 1},{0, 1},{1, 1}
@@ -32,6 +24,14 @@ enemy_pixel_maps = {
 	}
 }
 
+
+--------------------
+--ENEMY CLASS
+Enemy = {
+	pos = {x=0,y=0},
+	normal = {0,-1}
+	}
+
 function Enemy:new(o)
 	o = o or {}
 	setmetatable(o, self)
@@ -39,31 +39,52 @@ function Enemy:new(o)
 	self.pos.x, self.pos.y = 0, 0
 	self.normal = {0,-1}
 	self.direction = "up"
-	self.variant = "base";
-
+	self.variant = "base"
+	self.body = {};
 	return o
 end
 
+--MUST BE CALLED ON INITIALIZATION OR DATA WILL BE CORRUPTED
 function Enemy:init_values(pos_in, normal_in, variant_in)
 	self.pos.x, self.pos.y = pos_in[1], pos_in[2]
 	self.normal = normal_in
 	self.variant = variant_in
+	self.direction = get_direction(self.normal)
+	self.body = get_sprite(self.pos, self.variant, self.direction)
 end
 
+--
 function Enemy:show()
-
-	t_pixels = get_sprite(self.pos, self.variant, self.direction)	 
-
-	for k, v in pairs(t_pixels) do
+	-- 
+	for k, v in pairs(self.body) do
 		print("v: "..v[1])
 		love.graphics.rectangle("fill",v[1], v[2],1,1)
 	end
 end
 
+--function Enemy:check
+
+--Translates an enemies normal vector into a string based direction value 
+function get_direction(vec_in)
+	direction_out = "up"
+	if vec_in[1] == 0 then
+		if vec_in[2] == 1 then
+			direction_out = "down"
+		end
+	elseif vec_in[1] == 1 then
+		direction_out = "right"
+	elseif vec_in[1] == -1 then
+		direction_out = "left"
+	else
+		print("Incorrect vector was entered in get_direction()")
+	end
+
+	return direction_out 
+end
+-- Used to create the enemy's sprite from it's current state
 function get_sprite(enemy_pos,variant, direction)
 	pixels_out = {}
 
-	print("var: "..variant.." dir: "..direction)
 	for k, v in pairs(enemy_pixel_maps[variant][direction]) do
 		t_p = {x = enemy_pos.x + pixel_atlas[v][1],
 			   y = enemy_pos.y + pixel_atlas[v][2]
